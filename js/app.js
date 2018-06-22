fe737891a9655d3610bef1a5cde41faf13400b37
Chris Kozlowski <kozlowskicd@gmail.com>
2018-06-21 21:19:42 -0700

    pseudo code added

--- js/app.js
+++ js/app.js
@@ -43,6 +43,33 @@ var makeCssCards = function() {
 var displayCards = function() {
     {
         var ele = document.getElementById('card-deck');
-        ele.src = 'http://placehold.it/220x300/12345';
+        ele.src = 'http://placehold.it/150x200/12345';
+        for (var i = 0; i < 5; i++) {
+            var ele = document.getElementById('card' + String(i+1))
+            ele.src = 'http://placehold.it/150x200/12345';
+        }
     }
 }
+// Form Handler for username
+
+// Question and Answer Constructor--------------------------------
+//  needs to create question in the question-field id
+//  selects the answer card and 4 incorrect cards
+//  assigns cards to 'card1' through 'card5' ids
+//  contains explanations of answers to be displayed after answering
+
+// localStorage---------------------------------------------------
+//  storage for highscores, usernames, which question the user is on, user's current score, # of incorrect answers
+
+// Event Listener and Click Handler-------------------------------
+//  username form, clicking on cards
+
+// Answer-checking Logic-------------------------------------------
+//  if function to check if answer is correct
+//  while loop to track # of incorrect answers, game over if too many
+
+// DOM Manipulation------------------------------------------------
+//  display username
+//  display highscores
+//  display explanation after answering question
+
