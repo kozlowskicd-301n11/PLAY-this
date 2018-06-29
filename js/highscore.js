c4a24d0807895e1ba25f9410e48824e5a6f8bb3f
Chris Kozlowski <kozlowskicd@gmail.com>
2018-06-29 15:46:38 -0700

    add infobar

--- js/highscore.js
+++ js/highscore.js
@@ -1,1 +1,23 @@
 'use strict'
+function toggleSideBar(ref) {
+    ref.classList.toggle('active');
+    document.getElementById('sidebar').classList.toggle('active');
+    
+};
+
+function startHandler(){
+    event.preventDefault();
+    var userName = document.getElementById('user-name');
+    var getUserName = userName.playername.value;
+    var setName2Storage = localStorage.setItem('userName', JSON.stringify(getUserName));
+    window.location.href = "game.html";
+}
+
+var startButton = document.getElementById('submit').addEventListener('click', startHandler);
+
+
+function clearNameHandler(){
+    event.preventDefault();
+    localStorage.removeItem('userName');
+}
+var clearButtion = document.getElementById('clear-name').addEventListener('click', clearNameHandler);
