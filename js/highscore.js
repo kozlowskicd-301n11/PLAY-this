bd0da1b2e7549c39e203351c5dee8a91126fd791
Hai Le <haile@MacBook-Pro.localdomain>
2018-06-28 21:31:40 -0700

    updatedJS

--- js/highscore.js
+++ js/highscore.js
@@ -1,12 +1,38 @@
 'use strict'
-
-
 function toggleSideBar(ref) {
     ref.classList.toggle('active');
     document.getElementById('sidebar').classList.toggle('active');
     
 };
-// main ul li {
-//     display: inline-block;
-//     margin: 0px 4px;
-// }
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function startHandler(){
+    event.preventDefault();
+    var userName = document.getElementById('user-name');
+    var getUserName = userName.playername.value;
+    var setName2Storage = localStorage.setItem('userName', JSON.stringify(getUserName));
+    window.location.href = "game.html";
+}
+
+var startButton = document.getElementById('submit').addEventListener('click', startHandler);
+
+
+function clearNameHandler(){
+    event.preventDefault();
+    localStorage.removeItem('userName');
+}
+var clearButtion = document.getElementById('clear-name').addEventListener('click', clearNameHandler);
+// debugger
